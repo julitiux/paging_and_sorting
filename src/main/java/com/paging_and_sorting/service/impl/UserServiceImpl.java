185da8adc4aa5ca5e03effb4216f73356f45fcec
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-14 23:18:44 -0600

    implement method geAllUsersPaginated

--- src/main/java/com/paging_and_sorting/service/impl/UserServiceImpl.java
+++ src/main/java/com/paging_and_sorting/service/impl/UserServiceImpl.java
@@ -1,16 +1,24 @@
 package com.paging_and_sorting.service.impl;
 
 import com.paging_and_sorting.domain.User;
+import com.paging_and_sorting.repository.UserRepository;
 import com.paging_and_sorting.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+  @Autowired
+  private UserRepository userRepository;
+
   @Override
-    public Page<User> geAllUsersPaginated(Integer page, Integer size, String sortBy) {
-    return null;
+  public Page<User> geAllUsersPaginated(Integer page, Integer size, String sortBy) {
+    PageRequest pageRequest = PageRequest.of(page, size, Sort.by(sortBy));
+    return userRepository.findAll(pageRequest);
   }
 
 }
