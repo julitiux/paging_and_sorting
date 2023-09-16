2feab6fb9accdfb84afc921ea6afbd0dad87b0c5
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-16 02:53:14 -0600

    implement UserController, declaring a method called getPageUser; this method recive 3 params: page, size and sortBay and return an ResponseEntity<Page<User>>

---
+++ src/main/java/com/paging_and_sorting/webApi/UserController.java
@@ -0,0 +1,29 @@
+package com.paging_and_sorting.webApi;
+
+import com.paging_and_sorting.domain.User;
+import com.paging_and_sorting.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Objects;
+
+@Controller
+public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+  @RequestMapping(value = "/users", method = RequestMethod.GET)
+  public ResponseEntity<Page<User>> getPageUser(Integer page,
+                                                Integer size,
+                                                String sortBy) {
+    Page<User> userPage = userService.geAllUsersPaginated(page, size, sortBy);
+    return !Objects.isNull(userPage) ? new ResponseEntity<>(userPage, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+}
