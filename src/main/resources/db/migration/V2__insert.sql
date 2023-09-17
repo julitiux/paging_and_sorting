ef1dcad0e2b16281a9be3bf7f502a653b6102a7b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-17 01:00:58 -0600

    add script v2__insert

---
+++ src/main/resources/db/migration/V2__insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO people (id, username, name, lastname, password, date_created, last_updated)
+VALUES
+    ('1de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario1', 'Nombre1', 'Apellido1', 'contrasena1', NOW(), NOW()),
+    ('2de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario2', 'Nombre2', 'Apellido2', 'contrasena2', NOW(), NOW()),
+    ('3de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario3', 'Nombre3', 'Apellido3', 'contrasena3', NOW(), NOW()),
+    ('4de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario4', 'Nombre4', 'Apellido4', 'contrasena4', NOW(), NOW()),
+    ('5de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario5', 'Nombre5', 'Apellido5', 'contrasena5', NOW(), NOW()),
+    ('6de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario6', 'Nombre6', 'Apellido6', 'contrasena6', NOW(), NOW()),
+    ('7de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario7', 'Nombre7', 'Apellido7', 'contrasena7', NOW(), NOW()),
+    ('8de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario8', 'Nombre8', 'Apellido8', 'contrasena8', NOW(), NOW()),
+    ('9de52c61-9346-4b1b-a2a8-9d4876ad196a', 'usuario9', 'Nombre9', 'Apellido9', 'contrasena9', NOW(), NOW());
