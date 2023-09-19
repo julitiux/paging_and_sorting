932c6df1f7a0a3564d3019957d7b0ed38f493180
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-18 23:03:32 -0600

    change the object return, now return Page<UserDTO>

--- src/main/java/com/paging_and_sorting/webApi/UserController.java
+++ src/main/java/com/paging_and_sorting/webApi/UserController.java
@@ -1,7 +1,7 @@
 package com.paging_and_sorting.webApi;
 
-import com.paging_and_sorting.domain.User;
 import com.paging_and_sorting.service.UserService;
+import com.paging_and_sorting.webApi.domain.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -20,11 +20,13 @@ public class UserController {
   private UserService userService;
 
   @RequestMapping(value = "/users", method = RequestMethod.GET)
-  public ResponseEntity<Page<User>> getPageUser(@RequestParam Integer page,
-                                                @RequestParam Integer size,
-                                                @RequestParam String sortBy) {
-    Page<User> userPage = userService.geAllUsersPaginated(page, size, sortBy);
-    return !Objects.isNull(userPage) ? new ResponseEntity<>(userPage, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  public ResponseEntity<Page<UserDTO>> getPageUser(@RequestParam Integer page,
+                                                   @RequestParam Integer size,
+                                                   @RequestParam String sortBy) {
+    Page<UserDTO> userPage = userService.geAllUsersPaginated(page, size, sortBy);
+    return !Objects.isNull(userPage) ?
+      new ResponseEntity<>(userPage, HttpStatus.OK) :
+      new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
 }
